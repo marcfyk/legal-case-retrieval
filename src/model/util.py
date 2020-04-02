2e944c01a409476bd52b78ae4a9f9c5a5ff898ca
marcfyk <marcjohnfong@gmail.com>
2020-04-03 01:05:38 +0800

    add file util functions

--- src/model/util.py
+++ src/model/util.py
@@ -13,12 +13,20 @@ def stem(word):
     return porter_stemmer.stem(word.casefold())
 
 def has_any_alphanumeric(word):
+    '''
+    checks if a word contains >= 1 alphanumeric character.
+    '''
     for c in word:
         if c.isalnum():
             return True
     return False
 
 def accumulate(numbers):
+    '''
+    takes in a list X and returns a list Y,
+    where for a valid index i, X[i] == sum of all elements from Y[0] to Y[i] (inclusive)
+    [1,2,5] -> [1, 1, 3]
+    '''
     output = []
     total = 0
     for i in range(len(numbers)):
@@ -26,3 +34,29 @@ def accumulate(numbers):
         output.append(delta)
         total += delta
     return output
+
+def read_line_from_file(file_name, ptr):
+    '''
+    reads a line from a file given a ptr (offset).
+    '''
+    with open(file_name, 'a+') as f:
+        f.seek(ptr)
+        line = f.readline()
+    return line
+
+def get_line_pointers(file_name):
+    '''
+    returns a list of pointers (offsets) in a file,
+    where each pointer points to the start of a new line.
+    an empty file will return [0], and if the file doesn't exist,
+    the file will be created.
+    '''
+    ptrs = [0]
+    with open(file_name, 'a+') as f:
+        f.seek(0)
+        line = f.readline()
+        while line:
+            ptrs.append(f.tell())
+            line = f.readline()
+    return ptrs
+

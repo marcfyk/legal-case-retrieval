b37fe15c609c6ef2641da7cccb13bb1182f31990
marcfyk <marcjohnfong@gmail.com>
2020-04-06 19:50:32 +0800

    move fileposition class to term.py

--- src/model/term.py
+++ src/model/term.py
@@ -19,3 +19,13 @@ class Term:
         doc_frequency: {self.doc_frequency}\n
         file_position: ({self.file_position})
         '''
+
+class FilePosition:
+
+    def __init__(self, line=-1, offset=-1):
+        self.line = line
+        self.offset = offset
+
+    def __repr__(self):
+        return f'line: {self.line}, offset: {self.offset}'
+
